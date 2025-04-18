7f49370582d6c127641360077019929190565ecd
blakehulett7 <blake.hulett7@gmail.com>
2025-04-18 14:34:33 -0500

    one bite at a time

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io"
+	"mime"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -12,7 +13,7 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 	maxUploadSize := int64(1 << 30)
 
 	r.Body = http.MaxBytesReader(w, r.Body, maxUploadSize)
-	body, err := io.ReadAll(r.Body)
+	_, err := io.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, "Request body too large", http.StatusRequestEntityTooLarge)
 		return
@@ -34,6 +35,21 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
 	if err != nil {
 		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
+		return
+	}
+
+	metadata, err := cfg.db.GetVideo(videoID)
+	if metadata.UserID != userID {
+		respondWithJSON(w, http.StatusUnauthorized, struct{}{})
+		return
+	}
+
+	file, header, err := r.FormFile("video")
+	defer file.Close()
+
+	mediaType, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
+	if mediaType != "video/mp4" {
+		respondWithError(w, http.StatusBadRequest, "only takes jpegs or pngs", err)
 		return
 	}
 }
